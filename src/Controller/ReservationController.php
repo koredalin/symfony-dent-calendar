7fdcff30cf88a0b60dd4a88ca860b255c88ea8dd
Hristo Hristov <hristo.hristov@alphavision.bg>
2024-01-22 15:51:47 +0200

    Calendar actions

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -7,19 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\ReservationModel;
 
-class ReservedHourController extends AbstractController
+class ReservationController extends AbstractController
 {
     protected const IDS = [3,];
 
 
-    #[Route('/api/hours/{id<\d+>}', methods: ['GET'], name: 'api_hours_get_one')]
+    #[Route('/api/reservations/{id<\d+>}', methods: ['GET'], name: 'api_reservations_get_one')]
     public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO if no found record.
         if (!in_array($id, self::IDS)) {
-            $logger->warning('Reserved hour not found. Id: {hour}.', [
-                'hour' => $id,
+            $logger->warning('Reserved reservation not found. Id: {reservation}.', [
+                'reservation' => $id,
             ]);
             
             return $this->json(json_encode([]));
@@ -31,8 +32,8 @@ class ReservedHourController extends AbstractController
             'patient_name' => 'Ivan Ivanov',
             'patient_email' => 'i.ivanov@gmail.com',
             'patient_phone' => '+359889777111',
-            'hour' => 10,
-            'date' => '13-02-2024',
+            'reservation_hour' => 9,
+            'date' => '12-01-2024',
         ];
 
         return $this->json($song);
